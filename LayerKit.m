e61e28450c77effe3d813d9c515cc044888944b6
Blake Watters <blake@layer.com>
2015-03-16 12:49:26 -0700

    Add dummy file to trigger framework dependency linking

---
+++ LayerKit.m
@@ -0,0 +1,14 @@
+//
+//  LayerKit.m
+//  LayerKit
+//
+//  Created by Blake Watters on 3/16/16.
+//  Copyright (c) 2014 Layer. All rights reserved.
+//
+
+/**
+ This symbol is exported in order to cause CocoaPods to generate a target when
+ linking LayerKit as a transitive dependency. This prevents annoying linking issues
+ when working with LayerKit and Atlas in a Swift project.
+ */
+NSString *const LYRLayerKitDummySymbol = @"LYRLayerKitDummySymbol";
